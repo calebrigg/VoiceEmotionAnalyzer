9591fa3e5f7ff49f652c2a6cddf979f93e8a2027
Caleb Rigg <48843010+calebrigg@users.noreply.github.com>
2020-12-07 11:58:13 -0800

    Add files via upload

---
+++ getMeanLength.py
@@ -0,0 +1,30 @@
+import scipy.io.wavfile as wav
+import contextlib
+import os
+import sys
+from typing import Tuple
+
+def getLength(filepath):
+    fs, signal = wav.read(filepath)
+    return len(signal)
+
+
+class_labels: Tuple = ("RV", "RV2", "RV3", "RV4")
+
+totalDuration=0
+count=0
+cur_dir = os.getcwd()
+sys.stderr.write('curdir: %s\n' % cur_dir)
+os.chdir("AudioData")
+for i, directory in enumerate(class_labels):
+    sys.stderr.write("started reading folder %s\n" % directory)
+    os.chdir(directory)
+    for filename in os.listdir('.'):
+        if not filename.endswith(".wav"):
+            continue
+        filepath = os.getcwd() + '/' + filename
+        count+=1
+        totalDuration += getLength(filepath)
+    os.chdir('..')
+os.chdir(cur_dir)
+print(totalDuration/count)
